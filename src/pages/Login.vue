909d45a43d93c2b9143655a6414c791570de3289
배지원 <bjiwon2005@naver.com>
2018-12-25 20:48:05 +0900

    set srtucture

---
+++ src/pages/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <login-form></login-form>
+    </div>
+</template>
+
+<script>
+/*
+  written by Conan
+  2018-12-25
+*/
+import LoginForm from '../components/LoginForm';
+export default {
+  name: 'Login',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+</style>
