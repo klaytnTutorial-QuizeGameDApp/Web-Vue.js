c2b06bb9ced490473c365a10bc942f5e1a61a124
배지원(conan) <conan.bae@groundx.xyz>
2019-01-04 20:43:27 +0900

    set routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hoem from '@/pages/Home'
+import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+import Mypage from '@/pages/Mypage'
+import Apply from '@/pages/QuizApply'
+import Detail from '@/pages/QuizDetail'
+import Upload from '@/pages/QuizUpload'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [{
+export default new Router(
+  {
+    mode: 'history',
+  routes: [
+    {
     path: '/',
-    name: 'Hoem',
-    component: Hoem
-  }]
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/Mypage',
+    name: 'Mypage',
+    component: Mypage
+  },
+  {
+    path: '/Apply',
+    name: 'Apply',
+    component: Apply
+  },
+  {
+    path: '/Detail',
+    name: 'Detail',
+    component: Detail
+  },
+  {
+    path: '/Upload',
+    name: 'Upload',
+    component: Upload
+  },
+]
 })
